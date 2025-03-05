833d45cddd337953ff4f8481cf6dbef743260f0f
Sridhar Ratnakumar <srid@srid.ca>
2025-03-05 08:55:36 -0500

    ref more

--- src/Main.hs
+++ src/Main.hs
@@ -15,69 +15,40 @@ data Food
   = SalmonAtlantic
   | Butter
   | SausageDuBretonMildItalian -- https://www.dubreton.com/en-ca/products/all-natural/mild-italian-sausages
+  | KirklandGroundBeef
   deriving stock (Show, Eq, Ord)
-
--- Per 100g
-data Nutrition = Nutrition
-  { protein :: Double
-  , fat :: Double
-  , carbs :: Double
-  }
-  deriving stock (Show, Eq, Ord)
-
-instance Semigroup Nutrition where
-  (Nutrition p1 f1 c1) <> (Nutrition p2 f2 c2) = Nutrition (p1 + p2) (f1 + f2) (c1 + c2)
-
-instance Monoid Nutrition where
-  mempty = Nutrition 0 0 0
 
 foodNutrition :: Food -> Nutrition
 foodNutrition = \case
   SalmonAtlantic -> read "20p 13f 0c"
   Butter -> read "1p 81f 0c"
   SausageDuBretonMildItalian -> read "14p 22f 1c"
-
--- FIXME: grams should be part of out data
-scaleNutritionToGrams :: Nutrition -> Double -> Nutrition
-scaleNutritionToGrams (Nutrition p f c) grams = Nutrition (p * grams / 100) (f * grams / 100) (c * grams / 100)
-
-nutritionCalories :: Nutrition -> Double
-nutritionCalories (Nutrition p f c) = p * 4 + f * 9 + c * 4
-
-type Meal = [(Food, Double)]
-
-printMealMacros :: Meal -> IO ()
-printMealMacros meal = do
-  let totalNutrition = sumNutrition meal
-  putStrLn $ Shower.shower meal
-  putStrLn "Total Nutrition:"
-  -- TODO: Use round here
-  putStrLn $ "Protein: " ++ printf "%0.0f" (protein totalNutrition)
-  putStrLn $ "Fat: " ++ printf "%0.0f" (fat totalNutrition)
-  putStrLn $ "Carbs: " ++ printf "%0.0f" (carbs totalNutrition)
-  putStrLn $ "Calories: " ++ printf "%0.0f" (nutritionCalories totalNutrition)
-  putStrLn $ "Fat:Protein ratio: " ++ printf "%.2f" (fat totalNutrition / protein totalNutrition)
-
-sumNutrition :: Meal -> Nutrition
-sumNutrition meal =
-  foldl' (\acc (food, quantity) -> acc <> scaleNutritionToGrams food quantity) mempty $
-    first foodNutrition <$> meal
+  KirklandGroundBeef -> read "19p 15f 0c"
 
 salmonSausageMeal :: Meal
 salmonSausageMeal =
   [ (SalmonAtlantic, 300)
   , (SausageDuBretonMildItalian, 400)
-  , -- (Butter, 113)
-    (Butter, 113 / 2)
+  , (Butter, 113 / 2)
   ]
 
 main :: IO ()
 main = do
   Utf8.withUtf8 $ do
     printMealMacros salmonSausageMeal
+    printMealMacros
+      [ (SalmonAtlantic, 350)
+      , (SausageDuBretonMildItalian, 300)
+      , (Butter, 113 * 0.6)
+      ]
+    printMealMacros
+      [ (KirklandGroundBeef, 600)
+      , (Butter, 113)
+      ]
 
-read :: (HasCallStack, Read a) => String -> a
-read = fromMaybe (error "Bad input") . readMaybe
+-- ---------------------------------------------
+-- Internal
+-- ---------------------------------------------
 
 instance Read Nutrition where
   readsPrec _ input = case parse nutritionParser "<>" input of
@@ -101,3 +72,47 @@ nutritionParser = do
   void space
   carbs <- L.decimal <* char 'c'
   pure $ Nutrition {protein, fat, carbs}
+
+-- Per 100g
+data Nutrition = Nutrition
+  { protein :: Double
+  , fat :: Double
+  , carbs :: Double
+  }
+  deriving stock (Show, Eq, Ord)
+
+instance Semigroup Nutrition where
+  (Nutrition p1 f1 c1) <> (Nutrition p2 f2 c2) = Nutrition (p1 + p2) (f1 + f2) (c1 + c2)
+
+instance Monoid Nutrition where
+  mempty = Nutrition 0 0 0
+
+-- FIXME: grams should be part of out data
+scaleNutritionToGrams :: Nutrition -> Double -> Nutrition
+scaleNutritionToGrams (Nutrition p f c) grams = Nutrition (p * grams / 100) (f * grams / 100) (c * grams / 100)
+
+nutritionCalories :: Nutrition -> Double
+nutritionCalories (Nutrition p f c) = p * 4 + f * 9 + c * 4
+
+type Meal = [(Food, Double)]
+
+printMealMacros :: Meal -> IO ()
+printMealMacros meal = do
+  let totalNutrition = sumNutrition meal
+  putStrLn $ Shower.shower meal
+  putStrLn "Total Nutrition:"
+  -- TODO: Use round here
+  putStrLn $ "Protein: " ++ printf "%0.0f" (protein totalNutrition)
+  putStrLn $ "Fat: " ++ printf "%0.0f" (fat totalNutrition)
+  putStrLn $ "Carbs: " ++ printf "%0.0f" (carbs totalNutrition)
+  putStrLn $ "Calories: " ++ printf "%0.0f" (nutritionCalories totalNutrition)
+  putStrLn $ "Fat:Protein ratio: " ++ printf "%.2f" (fat totalNutrition / protein totalNutrition)
+  putStrLn "----"
+
+sumNutrition :: Meal -> Nutrition
+sumNutrition meal =
+  foldl' (\acc (food, quantity) -> acc <> scaleNutritionToGrams food quantity) mempty $
+    first foodNutrition <$> meal
+
+read :: (HasCallStack, Read a) => String -> a
+read = fromMaybe (error "Bad input") . readMaybe
